6c950ead3e219701d92b1247f5e50d045cc90155
hungndweq <thang7781@gmail.com>
2024-12-19 13:21:05 +0700

    Get product by ID

--- index.js
+++ index.js
@@ -9,15 +9,33 @@ app.get('/', (req, res) => {
     res.send("Hello from node API updated");
 });
 
-app.post('/api/products', async (req, res) => {
+app.get('/api/products', async (req, res) => {
     try {
-        const product = await Product.create(res.body);
+        const product = await Product.find({});
         res.status(200).json(product);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
-mongoose.connect("mongodb://localhost:27017/")
+
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+mongoose.connect("mongodb://localhost:27017/testing")
     .then(() => {
         console.log("Connected to database!")
         app.listen(3000, () => {
