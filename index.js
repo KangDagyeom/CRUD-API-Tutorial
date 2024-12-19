88cb634553b43a671b17a3a622e696a6f3811110
hungndweq <thang7781@gmail.com>
2024-12-19 22:00:20 +0700

    Upload routers an controllers

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Product = require('./models/products.model.js')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/products.model.js');
+const productRoute = require('./routes/product.route.js');
+const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/products', productRoute)
 
 app.get('/', (req, res) => {
     res.send("Hello from node API updated");
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const product = await Product.find({});
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+//Connection Promise
 mongoose.connect("mongodb://localhost:27017/testing")
     .then(() => {
         console.log("Connected to database!")
